3a15a20f8c0861f81dd2cca6f0c052018c90965a
lucas55i <lucas_55i@outlook.com>
2021-08-01 13:44:44 -0300

    Adesão de biografia as informações do jogador

--- src/jogadores/schemas/jogador.schema.ts
+++ src/jogadores/schemas/jogador.schema.ts
@@ -4,6 +4,7 @@ import * as mongoose from 'mongoose';
 export const JogadorSchema = new mongoose.Schema({
     name: String,
     region: String,
-    clubeAtual: String
+    clubeAtual: String,
+    biografia: String
 
 })
--- src/jogadores/shared/jogador.ts
+++ src/jogadores/shared/jogador.ts
@@ -1,22 +1,29 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Document } from "mongoose";
 
-export class Jogador extends Document{
+export class Jogador extends Document {
   @ApiProperty({
-    description:"Player's name",
-    example:"Sócrates Brasileiro Sampaio de Souza Vieira de Oliveira"
+    description: "Player's name",
+    example: "Sócrates Brasileiro Sampaio de Souza Vieira de Oliveira",
   })
   name: string;
 
   @ApiProperty({
-    description:"country where you were born",
-    example:"Brazil"
+    description: "country where you were born",
+    example: "Brazil",
   })
   region: string;
 
   @ApiProperty({
-    description:"Team where you played or are currently",
-    example:"corinthians"
+    description: "Team where you played or are currently",
+    example: "corinthians",
   })
   clubeAtual: string;
+
+  @ApiProperty({
+    description: "A short story about the player",
+    example:
+      "Sócrates Brazilian Sampaio de Souza Vieira de Oliveira, better known only as Sócrates and also referred to as Doutor Sócrates, Doutor or Magrão (Belém, February 19, 1954 — São Paulo, December 4, 2011), was a Brazilian soccer player and doctor.",
+  })
+  biografia: string;
 }
